ebcca1f1fb526efcca5f777b14200871546c830a
John Crocker <jcrocker@zenoss.com>
2014-01-25 11:04:29 -0600

    Enable minimap

--- js/impact_graph.js
+++ js/impact_graph.js
@@ -13,17 +13,22 @@ function ImpactDAG(attachPoint, impact_doc, /*optional*/ params) {
     var rootSVG = d3.select(attachPoint).append("svg");
     var graphSVG = rootSVG.append("svg").attr("width", "100%").attr("height", "100%").attr("class", "graph-attach");
     graphSVG.node().oncontextmenu = function(d) { return false; };
+    var minimapSVG = rootSVG.append("svg").attr("class", "minimap-attach");
+
     var graph = createGraphFromImpacts(impact_doc, params);
+
     var DAG = DirectedAcyclicGraph().animate(!lightweight);
+    var DAGMinimap = DirectedAcyclicGraphMinimap(DAG).width("19.5%").height("19.5%").x("80%").y("80%");
     // Attach the panzoom behavior
     var refreshViewport = function() {
         var t = zoom.translate();
         var scale = zoom.scale();
         graphSVG.select(".graph").attr("transform","translate("+t[0]+","+t[1]+") scale("+scale+")");
+        minimapSVG.select('.viewfinder').attr("x", -t[0]/scale).attr("y", -t[1]/scale).attr("width", attachPoint.offsetWidth/scale).attr("height", attachPoint.offsetHeight/scale);
         if (!lightweight) graphSVG.selectAll(".node text").attr("opacity", 3*scale-0.3);
     }
     var zoom = MinimapZoom().scaleExtent([0.001, 2.0]).on("zoom", refreshViewport);
-    zoom.call(this, rootSVG, null);
+    zoom.call(this, rootSVG, minimapSVG);
 
     // A function that resets the viewport by zooming all the way out
     var resetViewport = function() {
@@ -45,6 +50,9 @@ function ImpactDAG(attachPoint, impact_doc, /*optional*/ params) {
         var start = (new Date()).getTime();
         graphSVG.datum(graph).call(DAG);    // Draw a DAG at the graph attach
         console.log("draw graph", new Date().getTime() - start);
+        start = (new Date()).getTime();
+        minimapSVG.datum(graphSVG.node()).call(DAGMinimap);  // Draw a Minimap at the minimap attach
+        console.log("draw minimap", new Date().getTime() - start);
         console.log("draw complete, total time=", new Date().getTime() - begin);
     }
 
