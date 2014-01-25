2df3a3d90b2f6dbfdc8d5eed6f28c51318dcad20
John Crocker <jcrocker@zenoss.com>
2014-01-25 07:54:40 -0600

    Replace "report" rendering with impact rendering
    Removed tooltips/jquery references from graph.html.  These were
    causing crashes, and rather the root causing them I just removed
    them.

---
+++ js/impact_graph.js
@@ -0,0 +1,63 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zenoss
+ * Date: 1/24/14
+ * Time: 7:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function ImpactDAG(attachPoint, impact_doc, /*optional*/ params) {
+    var dag = this;
+    // Get the necessary parameters
+    var lightweight = params.lightweight ? true : false;
+
+    var rootSVG = d3.select(attachPoint).append("svg");
+    var graphSVG = rootSVG.append("svg").attr("width", "100%").attr("height", "100%").attr("class", "graph-attach");
+    graphSVG.node().oncontextmenu = function(d) { return false; };
+    var graph = createGraphFromImpacts(impact_doc, params);
+    var DAG = DirectedAcyclicGraph().animate(!lightweight);
+    // Attach the panzoom behavior
+    var refreshViewport = function() {
+        var t = zoom.translate();
+        var scale = zoom.scale();
+        graphSVG.select(".graph").attr("transform","translate("+t[0]+","+t[1]+") scale("+scale+")");
+        if (!lightweight) graphSVG.selectAll(".node text").attr("opacity", 3*scale-0.3);
+    }
+    var zoom = MinimapZoom().scaleExtent([0.001, 2.0]).on("zoom", refreshViewport);
+    zoom.call(this, rootSVG, null);
+
+    // A function that resets the viewport by zooming all the way out
+    var resetViewport = function() {
+        var curbbox = graphSVG.node().getBBox();
+        var bbox = { x: curbbox.x, y: curbbox.y, width: curbbox.width+50, height: curbbox.height+50};
+        scale = Math.min(attachPoint.offsetWidth/bbox.width, attachPoint.offsetHeight/bbox.height);
+        w = attachPoint.offsetWidth/scale;
+        h = attachPoint.offsetHeight/scale;
+        tx = ((w - bbox.width)/2 - bbox.x + 25)*scale;
+        ty = ((h - bbox.height)/2 - bbox.y + 25)*scale;
+        zoom.translate([tx, ty]).scale(scale);
+        refreshViewport();
+    }
+
+    // The main draw function
+    this.draw = function() {
+        console.log("draw begin")
+        var begin = (new Date()).getTime();
+        var start = (new Date()).getTime();
+        graphSVG.datum(graph).call(DAG);    // Draw a DAG at the graph attach
+        console.log("draw graph", new Date().getTime() - start);
+        console.log("draw complete, total time=", new Date().getTime() - begin);
+    }
+
+    // Call the draw function
+    this.draw();
+
+    // Start with the graph all the way zoomed out
+    resetViewport();
+
+    // Save important variables
+    this.attachPoint = attachPoint;
+    this.impact_doc = impact_doc;
+    this.DAG = DAG
+    this.graph = graph;
+    this.resetViewport = resetViewport;
+}
---
+++ js/impact_utils.js
@@ -0,0 +1,48 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zenoss
+ * Date: 1/24/14
+ * Time: 7:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var getImpactServices = function(id, callback, errback) {
+    d3.json(id, function(error, json) {
+        if (error)
+            errback(error);
+        else
+            callback(json);
+    });
+}
+
+var createGraphFromImpacts = function(impact_doc, params) {
+    console.log("Creating graph from impacts");
+
+    // Create nodes
+    console.info("Creating graph nodes");
+    var nodes = {};
+    var impact_nodes = impact_doc["nodes"];
+    for (var name in impact_nodes) {
+        nodes[name] = new Node(name);
+        nodes[name].impact_node = impact_nodes[name];
+    }
+
+    // Second link the nodes together
+    console.info("Linking graph nodes");
+    var edges = impact_doc["edges"];
+    for (var i in edges) {
+        edge = edges[i];
+        var toNode = nodes[edge["to"]];
+        var fromNode=nodes[edge["from"]];
+        toNode.addChild(fromNode);
+        fromNode.addParent(toNode);
+    }
+
+    // Create the graph and add the nodes
+    var graph = new Graph();
+    for (var id in nodes) {
+        graph.addNode(nodes[id]);
+    }
+
+    console.log("Done creating graph from reports");
+    return graph;
+}
